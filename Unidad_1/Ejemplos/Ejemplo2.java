35383acd8cf57d6f1707720d3e900a56dedd5c4e
ecasado <90179693+ecasadofp@users.noreply.github.com>
2022-09-22 09:36:16 +0200

    Create Ejemplo2.java

---
+++ Unidad_1/Ejemplos/Ejemplo2.java
@@ -0,0 +1,32 @@
+// No vale int -varY =3;
+// No vale int 1x=4;
+// Si vale int $varY=1;
+// No vale int public =1;
+// int x1= 10; int X1 = 20;
+// long x2=1000000000;
+// double x3= 4.55;
+// String x4 = "Buenos días";
+// char x5 = 'Y';
+// boolean x6 = true;
+// System.out.println(x1 > x2);
+
+public class Ejemplo2 {
+
+    public static void main(String[] args) {
+
+         int varY =3;
+         int x1=4;
+         int $varY=1;
+         int publico =1;
+         int x10= 10;
+         int x11 = 20;
+         long x2=1000000000;
+         double x3= 4.55;
+         String x4 = "Buenos días";
+         char x5 = 'Y';
+         boolean x6 = true;
+
+         System.out.println(x1 > x2);
+    }
+
+}
