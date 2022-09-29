127b5ae1ed4e297d20fc16a121ca55640549955e
ecasado <90179693+ecasadofp@users.noreply.github.com>
2022-09-29 12:54:38 +0200

    Add files via upload

---
+++ Unidad_2/Ejemplos/Ejemplo6.java
@@ -0,0 +1,11 @@
+// Muestra los números de 320 a 160
+// contando de 20 en 20 hacia atrás. (utilizando for)
+
+public class Ejemplo6 {
+
+    public static void main(String[] args) {
+
+        for(int i = 320; i >=160; i-=20)
+            System.out.println(i);
+    }
+}
