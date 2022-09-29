4666c3dd2f5497a27cfce4f1329c1de15777b024
ecasado <90179693+ecasadofp@users.noreply.github.com>
2022-09-29 12:42:42 +0200

    Add files via upload

---
+++ Unidad_2/Ejemplos/Ejemplo7.java
@@ -0,0 +1,15 @@
+// Muestra los multiplos de 5 de 0 a 100 (utilizando while)
+
+public class Ejemplo7 {
+
+    public static void main(String[] args) {
+
+        int multiplo = 0;
+
+        while(multiplo <=100){
+            System.out.println(multiplo);
+            multiplo += 5;
+        }
+
+    }
+}
